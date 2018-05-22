4ad0c085961033ca711b8f9bf0b93497f78d315c
Arthur Jen <arthurljen@gmail.com>
2018-05-22 15:21:17 -0700

    ingredient selector is now able to randomly fill up to 10 ingredients

--- js/IngredientSelector.js
+++ js/IngredientSelector.js
@@ -1,13 +1,14 @@
 /* exported IngredientSelectorDisplay */
-/* globals Ingredient */
+/* globals Ingredient, getRandomInt */
 
 'use strict';
 
 const ingredientSelectorTemplate = document.getElementById('ingredient-selector-template');
 
 class IngredientSelectorDisplay {
-    constructor(recipe) {
+    constructor(recipe, allIngredients) {
         this.recipe = recipe;
+        this.allIngredients = allIngredients;
     }
 
     render() {
@@ -15,14 +16,29 @@ class IngredientSelectorDisplay {
         const recipeName = dom.getElementById('recipe-name');
         recipeName.textContent = this.recipe.name;
 
-
         //Find out how many ingredients the recipe specifies, and add them to arrayOf10Ingredients
-        //Calculate how many more "dummy" ingredients are needed
-        //Randomly select enough "dummy" ingredients and add them into arrayOf10Ingredients
-        //Shuffle arrayOf10Ingredients
-        const arrayOf10Ingredients = [new Egg(10), new Egg(9), new Egg(8), new Egg(7), new Egg(6), new Egg(5), new Egg(4), new Egg(3), new Egg(2), new Egg(1)];
+        let arrayOf10Ingredients = this.recipe.ingredients;
 
-        
+        //Calculate how many more "dummy" ingredients are needed
+        const remainingIngredients = 10 - arrayOf10Ingredients.length;
+        this.allIngredients = [new Cheese(0), new Bacon(0)];
+        console.log(this.allIngredients);
+
+        //Randomly select enough "dummy" ingredients and add them into arrayOf10Ingredients
+        for(let j = 0; j < remainingIngredients; j++) {
+            
+            let index = getRandomInt(this.allIngredients.length);
+            console.log('Index: ', index);
+            let dummyIngredient = this.allIngredients[index];
+            console.log('Dummy: ', dummyIngredient);
+            arrayOf10Ingredients.push(dummyIngredient);
+        }
+
+
+        //Shuffle arrayOf10Ingredients
+        console.log(arrayOf10Ingredients);
+
+
         const ingredientSelectorSection = dom.getElementById('ingredient');
         for(let i = 0; i < 10; i++) {
             const ingredientSelectorComponent = new Ingredient(arrayOf10Ingredients[i]);
