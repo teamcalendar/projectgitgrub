9bc18cf620ae33cbbbf1dcf0c68cdf89a3b5b812
Ryan Luras <ryanluras@gmail.com>
2018-05-22 14:14:02 -0700

    working on player display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('app.js is loading properly');
+
+var chefSelect = document.getElementById('chef-select'); 
+
+chefSelect.addEventListener('submit', collectChefChoice);
+function collectChefChoice (event) {
+    event.preventDefault();
+    var playerChef = event.target.src;
+    var userName = event.target.value;
+    console.log('userName:', userName);
+    console.log('userImageChoice: ', playerChef);
+}
