a578996147d32b40a1da469b181e212b9472dfc4
Mark Myers <markalope@gmail.com>
2018-05-22 14:11:28 -0700

    changes to resolve merge conflicts

--- js/menu.js
+++ js/menu.js
@@ -1,34 +1,5 @@
 console.log('menu.js is loading properly');
 
-<<<<<<< HEAD
-
-
-$(function() {
-
-    var special = ['reveal', 'top', 'boring', 'perspective', 'extra-pop'];
-  
-    // Toggle Nav on Click
-    $('#hearts a').click(function() {
-  
-        var transitionClass = $(this).data('transition');
-    
-        if ($.inArray(transitionClass, special) > -1) {
-            $('body').removeClass();
-            $('body').addClass(transitionClass);
-        } else {
-            $('body').removeClass();
-            $('#site-canvas').removeClass();
-            $('#site-canvas').addClass(transitionClass);
-        }
-    
-        $('#site-wrapper').toggleClass('show-nav');
-    
-        return false;
-  
-    });
-  
-});
-=======
 const menu = document.querySelector('#toggle');  
 const menuItems = document.querySelector('#overlay');  
 const menuContainer = document.querySelector('.menu-container');  
@@ -43,4 +14,3 @@ function toggleMenu(e) {
 }
 
 menu.addEventListener('click', toggleMenu, false);
->>>>>>> master
