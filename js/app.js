4bae3c13312bb276225114a414ad10bb3600dbe6
Ryan Luras <ryanluras@gmail.com>
2018-05-22 15:11:37 -0700

    got username and image form retrieved

--- js/app.js
+++ js/app.js
@@ -2,11 +2,19 @@ console.log('app.js is loading properly');
 
 var chefSelect = document.getElementById('chef-select'); 
 
-chefSelect.addEventListener('submit', collectChefChoice);
+chefSelect.addEventListener('click', collectChefChoice);
 function collectChefChoice (event) {
     event.preventDefault();
     var playerChef = event.target.src;
-    var userName = event.target.value;
-    console.log('userName:', userName);
     console.log('userImageChoice: ', playerChef);
 }
+
+
+var playerName = document.getElementById('player-name');
+playerName.addEventListener('submit', playerNameSubmit);
+function playerNameSubmit (event) {
+    event.preventDefault();
+    var userName = event.target.username.value;
+    console.log('userName:', userName);
+}
+
