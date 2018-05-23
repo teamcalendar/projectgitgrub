ff5316e262ab8502081735e1cf42bbd8a35b8194
Ryan Luras <ryanluras@gmail.com>
2018-05-23 09:20:08 -0700

    judge showing

--- js/judgedisplay.js
+++ js/judgedisplay.js
@@ -6,13 +6,16 @@
 const judgeDisplayTemplate = document.getElementById('judge-display-template');
 
 class JudgeDisplay {
-    constructor() {
+    constructor(score) {
+        this.score = score;
+        this.judgeState = '../images/Judge-default.png';
 
     }
 
     render() {
-        const dom = judgeDisplayTemplate.content;
-        
+        const dom = judgeDisplayTemplate.content.cloneNode(true);
+        const judgeImage = dom.querySelector('img');
+        judgeImage.src = this.judgeState;
         return dom;
     }
 }
